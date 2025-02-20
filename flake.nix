7ac0ac153ca977ff80e37b34c1387c0cf46cd320
Luciism <lucism.dev@gmail.com>
2025-02-20 20:48:20 +1300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix/2f0cc0c110c25804cd2f6c167ab66f567941452c";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # sops-nix.url = "github:Mic92/sops-nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      spicetify-nix,
+      home-manager,
+      # sops-nix,
+    }@inputs:
+    {
+      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+      nixosConfigurations = {
+        local = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/lucism-desktop/configuration.nix
+            ./hosts/lucism-desktop/spicetify.nix
+            # sops-nix.nixosModules.sops
+          ];
+        };
+      };
+    };
+}
---
+++ hosts/lucism-desktop/home.nix
@@ -0,0 +1,84 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  home.username = "lucism";
+  home.homeDirectory = "/home/lucism";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    tmux
+    gimp
+    libreoffice
+    discord
+    (vesktop.overrideAttrs (
+      finalAttrs: previousAttrs: {
+        desktopItems = [
+          ((builtins.elemAt previousAttrs.desktopItems 0).override { icon = "discord"; })
+        ];
+      }
+    ))
+    firefox-devedition
+    figma-linux
+    # openvpn
+    # openvpn3
+    docker-client
+    vscode
+    # onedrivegui
+    # spotify
+    # spicetify-cli
+    # thunderbird
+    # discord
+  ];
+  # Secrets
+  # sops.secrets."cloudflared/cert".path = "${config.home.homeDirectory}/.cloudflared/cert.pem";
+
+  # For nixd LSP to recognize what nixpkgs version I use
+  nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # xdg.desktopEntries = {
+  #   vesktop = {
+  #     name = "Discord";
+  #     genericName = "Discord";
+  #     exec = "vesktop";
+  #     terminal = false;
+  #     categories = [
+  #       "Application"
+  #       "Messaging"
+  #       "Social"
+  #     ];
+  #   };
+  # };
+  #
+  home.sessionPath = [
+    "$HOME/.local/bin"
+  ];
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
