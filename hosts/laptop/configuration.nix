ea19ada8fa94ae393fb90f656eba3e0889ccfdfa
Luciism <lucism.dev@gmail.com>
2025-11-19 19:24:36 +1300

    chore(laptop): change hostname to "laptop"

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -4,8 +4,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "local"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "laptop"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
